32f45cebd9256dc6ced5d3dced4ee9db498750bf
Yehor Skibnevskyi <105387497+Skyhor@users.noreply.github.com>
2022-10-09 13:01:27 +0200

    1233

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,12 @@
+const startBtn = document.querySelector('[data-start]');
+const stopBtn = document.querySelector('[data-stop]');
+const body = document.querySelector('body');
+console.log(body);
+startBtn.addEventListener('click', ChangColorOnClick);
 
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
+function ChangColorOnClick() {
+  console.log('123');
+}
