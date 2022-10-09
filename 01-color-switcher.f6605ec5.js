d8160e2f95d829532bb2a6899921894de0c9a80c
Skyhor <105387497+Skyhor@users.noreply.github.com>
2022-10-09 11:01:52 +0000

    Deploying to gh-pages from @ Skyhor/goit-js-hw-09@32f45cebd9256dc6ced5d3dced4ee9db498750bf 🚀

---
+++ 01-color-switcher.f6605ec5.js
@@ -0,0 +1,2 @@
+!function(){var e=document.querySelector("[data-start]"),o=(document.querySelector("[data-stop]"),document.querySelector("body"));console.log(o),e.addEventListener("click",(function(){console.log("123")}))}();
+//# sourceMappingURL=01-color-switcher.f6605ec5.js.map
---
+++ 01-color-switcher.f9307070.js
@@ -0,0 +1,2 @@
+const e=document.querySelector("[data-start]"),o=(document.querySelector("[data-stop]"),document.querySelector("body"));console.log(o),e.addEventListener("click",(function(){console.log("123")}));
+//# sourceMappingURL=01-color-switcher.f9307070.js.map
