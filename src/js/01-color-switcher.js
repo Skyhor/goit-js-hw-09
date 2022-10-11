fc4422b5c173e6741174b0513c409eb3740edeb6
Yehor Skibnevskyi <105387497+Skyhor@users.noreply.github.com>
2022-10-12 00:41:00 +0200

    Finish

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -7,11 +7,13 @@ function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
 ref.startBtn.addEventListener('click', () => {
+  ref.startBtn.disabled = true;
   timerId = setInterval(() => {
     document.body.style.background = getRandomHexColor();
   }, 1000);
 });
 
 ref.stopBtn.addEventListener('click', () => {
+  ref.startBtn.disabled = false;
   clearInterval(timerId);
 });
