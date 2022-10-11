6ad479f192b294053b524d9c1549dca2ed9c773b
Skyhor <105387497+Skyhor@users.noreply.github.com>
2022-10-11 22:41:26 +0000

    Deploying to gh-pages from @ Skyhor/goit-js-hw-09@fc4422b5c173e6741174b0513c409eb3740edeb6 🚀

---
+++ 01-color-switcher.f941e840.js
@@ -0,0 +1,2 @@
+const t={startBtn:document.querySelector("[data-start]"),stopBtn:document.querySelector("[data-stop]"),body:document.querySelector("body")};t.startBtn.addEventListener("click",(()=>{t.startBtn.disabled=!0,timerId=setInterval((()=>{document.body.style.background=`#${Math.floor(16777215*Math.random()).toString(16)}`}),1e3)})),t.stopBtn.addEventListener("click",(()=>{t.startBtn.disabled=!1,clearInterval(timerId)}));
+//# sourceMappingURL=01-color-switcher.f941e840.js.map
--- 01-color-switcher.fb56973f.js
+++
@@ -1,2 +0,0 @@
-!function(){var t={startBtn:document.querySelector("[data-start]"),stopBtn:document.querySelector("[data-stop]"),body:document.querySelector("body")};t.startBtn.addEventListener("click",(function(){timerId=setInterval((function(){document.body.style.background="#".concat(Math.floor(16777215*Math.random()).toString(16))}),1e3)})),t.stopBtn.addEventListener("click",(function(){clearInterval(timerId)}))}();
-//# sourceMappingURL=01-color-switcher.fb56973f.js.map
