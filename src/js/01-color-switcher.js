62e27649195f1d3045e4b396e5c873488a52fbc5
Yehor Skibnevskyi <105387497+Skyhor@users.noreply.github.com>
2022-10-13 23:41:54 +0200

    Update 01-color-switcher.js

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,19 +1,17 @@
-const ref = {
-  startBtn: document.querySelector('[data-start]'),
-  stopBtn: document.querySelector('[data-stop]'),
-  body: document.querySelector('body'),
-};
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
-ref.startBtn.addEventListener('click', () => {
+const startBtn = document.querySelector('[data-start]');
+const stopBtn = document.querySelector('[data-stop]');
+const body = document.querySelector('body');
+
+startBtn.addEventListener('click', () => {
   timer = setInterval(() => {
     document.body.style.background = getRandomHexColor();
   }, 1000);
-  ref.startBtn.disabled = true;
+  startBtn.disabled = true;
 });
-
-ref.stopBtn.addEventListener('click', () => {
+stopBtn.addEventListener('click', () => {
   clearInterval(timer);
-  ref.startBtn.disabled = false;
+  startBtn.disabled = false;
 });
